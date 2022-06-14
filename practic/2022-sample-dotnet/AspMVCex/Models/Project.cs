ac51ec4c0d5f0221fec562744674412c931e3b7b
Alice laptop <53339016+913AliceHincu@users.noreply.github.com>
2022-06-14 23:06:35 +0300

    2022 dot net

---
+++ practic/2022-sample-dotnet/AspMVCex/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspMVCex.Models
+{
+    public class Project
+    {
+        public int id { get; set; }
+        public int projectManagerID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string members { get; set; }
+    }
+}
